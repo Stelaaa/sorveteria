fed8a2eade7e11af6171579c34f3afc8f1e5c90e
gabrielgregorio-desouza <103073023+gabrielgregorio-desouza@users.noreply.github.com>
2023-11-08 20:23:25 -0300

    Update HelloApplication.java

--- src/main/java/com/uefs/sigsorveterias/HelloApplication.java
+++ src/main/java/com/uefs/sigsorveterias/HelloApplication.java
@@ -9,18 +9,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication /*extends Application*/ {
-    /*@Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }*/
+public class HelloApplication  {
+   
 
     public static void main(String[] args) throws Exception{
-        //launch();
+       
         DAO.getClienteDAO().create(new Cliente("A", "123"));
         DAO.getClienteDAO().create(new Cliente("B", "456"));
         Cliente cliente = DAO.getClienteDAO().create(new Cliente("C", "789"));
