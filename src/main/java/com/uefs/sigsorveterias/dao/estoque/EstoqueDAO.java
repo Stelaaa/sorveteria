f549cf6a3676a02657a35de7f645d85fd6b4f247
gabrielgregorio-desouza <103073023+gabrielgregorio-desouza@users.noreply.github.com>
2023-11-08 20:32:35 -0300

    Update EstoqueDAO.java

--- src/main/java/com/uefs/sigsorveterias/dao/estoque/EstoqueDAO.java
+++ src/main/java/com/uefs/sigsorveterias/dao/estoque/EstoqueDAO.java
@@ -3,7 +3,6 @@ package com.uefs.sigsorveterias.dao.estoque;
 import com.uefs.sigsorveterias.dao.CRUD;
 import com.uefs.sigsorveterias.model.Estoque;
 
-import java.util.List;
 
 public interface EstoqueDAO extends CRUD<Estoque, Exception> {
 
